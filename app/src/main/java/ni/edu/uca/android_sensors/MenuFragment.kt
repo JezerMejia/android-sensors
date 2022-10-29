184b2c6f612af551fd7f5860a506a09264cbb044
Armando <amezal01@hotmail.com>
2022-10-29 15:14:27 -0600

    navigation added

---
+++ app/src/main/java/ni/edu/uca/android_sensors/MenuFragment.kt
@@ -0,0 +1,41 @@
+package ni.edu.uca.android_sensors
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import ni.edu.uca.android_sensors.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment() {
+    private lateinit var binding: FragmentMenuBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        layoutInflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMenuBinding.inflate(layoutInflater, container, false)
+
+        //Button navigation
+        binding.btnLightSensor.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_lightSensorFragment)
+        }
+        binding.btnXSensor.setOnClickListener {
+            // Cambiar param del navigate
+            findNavController().navigate(R.id.action_menuFragment_to_lightSensorFragment)
+        }
+        binding.btnYSensor.setOnClickListener {
+            // Cambiar param del navigate
+            findNavController().navigate(R.id.action_menuFragment_to_lightSensorFragment)
+        }
+
+        return binding.root
+    }
+}
